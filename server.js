89c4650b2a38147afcaf9eaa5458d9027cbcff37
Anubhav-MI <120900218+Anubhav-MI@users.noreply.github.com>
2024-03-15 18:22:33 +0530

    dasboard completed

--- server.js
+++ server.js
@@ -101,6 +101,42 @@ app.put("/order-status/:orderId", async (req, res) => {
   }
 });
 
+//update user password
+app.put("/updatepassword/:userId", async (req, res) => {
+  try {
+    // console.log("updatepassword route hit");
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const { name, email, password, newpassword } = req.body;
+    // console.log("newpassword", newpassword);
+    const hashedPassword = await bcrypt.hash(newpassword, 10);
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(400).send("Current password is incorrect");
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        name: name || user.name,
+        email: email || user.email,
+        password: hashedPassword || user.password,
+      },
+      { new: true }
+    );
+    console.log("updatedUser", updatedUser);
+    res.status(200).send({ message: "Password updated successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 //add actegory
 app.post("/createcategory", async (req, res) => {
   const { name } = req.body;
