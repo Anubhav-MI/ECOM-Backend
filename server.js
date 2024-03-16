caf6dd698df565a23fba8da603fbc364af7bc609
Anubhav-MI <120900218+Anubhav-MI@users.noreply.github.com>
2024-03-16 16:56:51 +0530

    restored

--- server.js
+++ server.js
@@ -11,24 +11,17 @@ import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import braintree from "braintree";
 import dotenv from "dotenv";
-import path from "path";
 dotenv.config();
 const app = express();
 
 app.use(cookieParser());
-app.use(
-  express.static(path.join(new URL(".", import.meta.url).pathname, "build"))
-);
+
 const port = process.env.PORT || 3001;
 const DATABASE_URL = process.env.DATABASE_URL;
 
 mongoose.connect(DATABASE_URL);
 app.use(bodyParser.json());
 app.use(cors());
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 app.get("/products/get", (req, res) => {
   Products.find()
